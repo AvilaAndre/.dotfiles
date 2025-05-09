373dc8b908b58c4a197400b17afe51a9062559ad
AvilaAndre <avila_andre14@hotmail.com>
2025-05-09 14:06:06 +0100

    feat(zsh): add more aliases for git

--- zsh/.config/zsh/git.zsh
+++ zsh/.config/zsh/git.zsh
@@ -3,12 +3,18 @@ alias gs="git status --short"
 
 alias ga="git add"
 alias gap="git add --patch"
+
 alias gc="git commit"
+alias gca='gc --amend --no-edit'
+alias gce='gc --amend'
 
 alias gp="git push"
 alias gu="git pull"
 
-alias gl="git log"
+alias gl='git log --oneline'
+alias glg='git log --graph --all --pretty=format:"%C(magenta)%h %C(white) %an  %ar%C(blue)  %D%n%s%n"'
+
 alias gb="git branch"
+alias gba="gb --all"
 
 alias gcl="git clone"
